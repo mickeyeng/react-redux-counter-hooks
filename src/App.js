80cdd2bdfa9175b6de422f50e866f1a31a377b7d
Mickey English <mickeyeng92@gmail.com>
2020-06-07 23:24:08 +0000

    Added hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import './App.css';
-import {connect} from "react-redux";
+// import {connect} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import {increment, decrement} from "./redux/Count"
 
 function App(props) {
+  const count = useSelector(state => state);
+  const dispatch = useDispatch()
   console.log(props)
+  
+  
+  
   return (
     <div className="App">
       <h1>Counter</h1>
-      <h2>{props.count}</h2>
-      <button onClick={props.increment}>+1</button>
-      <button onClick={props.decrement}>-1</button>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment)}>+1</button>
+      <button onClick={() => dispatch(decrement)}>-1</button>
     </div>
   );
 }
 
 
-const mapStateToProps = (state) => {
-  return {
-    count: state
-  }
-}
 
-const mapDispatchToProps = {
-  increment,
-  decrement
-  
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default (App)
 
 
 
